601e2e76636727b83946e050fe18ee5da839a756
Игорёха <leonidtagin401@gmail.com>
2024-10-26 06:38:12 +1100

    Initial commit

---
+++ modul_4_1.py
@@ -0,0 +1,3 @@
+from fake_math import divide as fake_math
+
+from true_math import divide as true_math
---
+++ true_math.py
@@ -0,0 +1,12 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    return first / second
+    float('inf')
+    return inf
+result3 = divide(36, 6)
+result4 = divide(5, 0)
+
+print(result3)
+print(result4)
+
